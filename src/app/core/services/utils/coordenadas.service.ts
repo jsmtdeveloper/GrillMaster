b6905ea16bd9d436143cf8ee06650f41474212b6
jsmtdeveloper <jsmtdeveloper@gmail.com>
2022-09-06 23:46:34 +0200

    eliminar video al ocultar

--- src/app/core/services/utils/coordenadas.service.ts
+++ src/app/core/services/utils/coordenadas.service.ts
@@ -6,9 +6,23 @@ import { Injectable } from '@angular/core';
 export class CoordenadasService {
   public latitude: number = 39.5065;
   public longitude: number = -0.41009;
-
+  public show: boolean = false;
   public get currentPlace(): string {
     console.log(`latitude:'${this.latitude.toString()}'; longitude:'${this.longitude.toString()}'`);
     return `latitude:'${this.latitude.toString()}'; longitude:'${this.longitude.toString()}'`;
+  }
+
+  onToggleShow() {
+    if (this.show) {
+      var element = document.getElementsByTagName('video'),
+        index;
+      if (element != null) {
+        for (index = element.length - 1; index >= 0; index--) {
+          element[index].parentNode?.removeChild(element[index]);
+        }
+      }
+    }
+
+    this.show = !this.show;
   }
 }
